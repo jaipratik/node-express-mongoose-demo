b90238f8a9c84477d33e08b06771654c67c62624
jpca999 <jpca999@gmail.com>
2013-09-18 19:13:27 -0700

    --

--- config/config.js
+++ config/config.js
@@ -24,7 +24,7 @@ module.exports = {
     facebook: {
       clientID: "363909887042099",
       clientSecret: "c034c5380af10fe4782bb7bb544680bd",
-      callbackURL: "http://www.localhost:80/auth/facebook/callback"
+      callbackURL: "http://www.indiadilse.com/auth/facebook/callback"
     },
     twitter: {
       clientID: "NhNCwt2r41x3AAG9OSH5yQ",
@@ -55,14 +55,14 @@ module.exports = {
       name: 'Nodejs Express Mongoose Demo'
     },
     facebook: {
-      clientID: "363909887042099",
-      clientSecret: "c034c5380af10fe4782bb7bb544680bd",
-      callbackURL: "http://www.localhost:80/auth/facebook/callback"
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
     },
     twitter: {
-      clientID: "NhNCwt2r41x3AAG9OSH5yQ",
-      clientSecret: "T7rsTCFyKOyby7aeXtt92HK9NizJVBfH3LoV6uVp3A",
-      callbackURL: "http://www.indiadilse.com/auth/twitter/callback"
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
     },
     github: {
       clientID: 'APP_ID',
@@ -88,14 +88,14 @@ module.exports = {
       name: 'Nodejs Express Mongoose Demo'
     },
     facebook: {
-      clientID: "363909887042099",
-      clientSecret: "c034c5380af10fe4782bb7bb544680bd",
-      callbackURL: "http://www.localhost:80/auth/facebook/callback"
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
     },
     twitter: {
-      clientID: "NhNCwt2r41x3AAG9OSH5yQ",
-      clientSecret: "T7rsTCFyKOyby7aeXtt92HK9NizJVBfH3LoV6uVp3A",
-      callbackURL: "http://www.indiadilse.com/auth/twitter/callback"
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
     },
     github: {
       clientID: 'APP_ID',
