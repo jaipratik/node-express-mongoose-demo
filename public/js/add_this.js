8b9e7c600e00138bc83afcb5d09d42794b0ab858
jpca999 <jpca999@gmail.com>
2013-09-21 05:22:16 -0700

    add this buttons working

---
+++ public/js/add_this.js
@@ -0,0 +1,17 @@
+
+  addthis.layers({
+  'theme' : 'gray',
+  'share' : {
+  'position' : 'left',
+  'numPreferredServices' : 4
+  },
+  'follow' : {
+  'services' : [
+  {'service': 'facebook', 'id': 'pages/Indiadilse/383883588358279'},
+  {'service': 'twitter', 'id': 'indiadilse1'},
+  {'service': 'google_follow', 'id': '114814969625534275747/posts'}
+  ]
+  },
+  'whatsnext' : {},
+  'recommended' : {}
+  });
